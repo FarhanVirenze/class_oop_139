2e717c2fb7471c3140caac6fb73617343354f4a0
FarhanVirenze <farhanvirenze18@gmail.com>
2023-04-10 11:10:32 +0700

    perbaikan method tampil matakuliah

--- class_oop_139/class_oop_139.cpp
+++ class_oop_139/class_oop_139.cpp
@@ -29,9 +29,9 @@ public:
         cin >> sks;
     }
     void tampil() {
-        cout << "Kode Matakuliah : ";
-        cout << "Nama Matakuliah : ";
-        cout << "SKS : ";
+        cout << "\nKode Matakuliah : ";
+        cout << "\nNama Matakuliah : ";
+        cout << "\nSKS : ";
     }
 };
 
